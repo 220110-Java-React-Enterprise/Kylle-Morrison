32e51773f9e1822197efafd7563a7e247b366a2d
Kai Morrison <morrisonksa@gmail.com>
2022-01-18 11:24:11 -0500

    Updates to Bubble Sort

--- practice-coding/BubbleSort/src/main/java/IntegerBubbleSort.java
+++ practice-coding/BubbleSort/src/main/java/IntegerBubbleSort.java
@@ -10,7 +10,7 @@ public class IntegerBubbleSort {
     public static void sort(int [] array) {
         int temp;
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - 1; j++) {
                 if (array[j] > array[j+1]) {
                     temp = array[j];
