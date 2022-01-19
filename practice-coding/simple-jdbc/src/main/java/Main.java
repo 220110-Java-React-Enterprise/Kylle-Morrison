046d7c7d21e440484f1229398af061961d8f2a49
Kai Morrison <morrisonksa@gmail.com>
2022-01-19 14:04:18 -0500

    updates

---
+++ practice-coding/simple-jdbc/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String ...args) {
+        //We need connection to mariaDB
+        //We will need an interface for CRUD
+        //we will implement this interface on our repos
+        //we need to store the database credentials safely and securely
+        
+    }
+}
